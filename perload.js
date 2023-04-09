6e1a8d1ea93ff3101104fd434294623f76780f15
etwxr9 <etwxr9>
2023-04-09 09:37:37 +0800

    test 4.9

--- perload.js
+++ perload.js
@@ -4,6 +4,7 @@ const {
 } = require("electron");
 
 
+
 // // Expose protected methods that allow the renderer process to use
 // // the ipcRenderer without exposing the entire object
 // contextBridge.exposeInMainWorld(
